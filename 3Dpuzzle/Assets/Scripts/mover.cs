1c067e395fd125127119d6a008c5abe285801c20
OzzyOsbourne <andreeamanole2020@gmail.com>
2023-05-09 17:24:34 +0300

    Added cinemachine

--- 3Dpuzzle/Assets/Scripts/mover.cs
+++ 3Dpuzzle/Assets/Scripts/mover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 1.4f;
+    [SerializeField] float moveSpeed = 8f;
     // Start is called before the first frame update
     void Start()
     {
